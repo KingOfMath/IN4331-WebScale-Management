2cdf7d4c6c71e6779238620900d114940387f232
Jason Xie <elitecqc@gmail.com>
2020-06-14 23:41:19 +0200

    Clean up old files

--- StockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yugabyte.springdemo.repository;
-
-import com.yugabyte.springdemo.model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
